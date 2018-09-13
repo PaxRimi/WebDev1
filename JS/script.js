2f14ff0278f4bcf5e5e724dcd7ff76e6a0adebab
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-09-13 19:25:51 +0200

    Add validation for 3,4,5 form

--- JS/script.js
+++ JS/script.js
@@ -131,4 +131,107 @@ document.addEventListener("DOMContentLoaded", function(event) {
             e.preventDefault();
         }
     }
+
+    //Third form validation
+    
+    const signNick = document.getElementById("name3");
+    const signNickFalse = document.getElementById("signNick");
+    const signPass = document.getElementById("password2");
+    const signPassFalse = document.getElementById("signPassword");
+    
+    
+    const signSubmit = document.getElementById("signSub");
+
+    signNick.addEventListener("keypress", () => {
+       signNickFalse.style.display = "none";
+    });
+
+    signPass.addEventListener("keypress", () => {
+        signPassFalse.style.display = "none";
+    });
+    
+    signSubmit.addEventListener("click", checkSignForm);
+    
+    function checkSignForm(e) {
+        let stop = false;
+
+        if(signNick.value.length === 0){
+            signNickFalse.style.display = "block";
+            stop = true;
+        }
+
+        if(signPass.value.length === 0){
+            signPassFalse.style.display = "block";
+            stop = true;
+        }
+
+        if( stop === true ){
+            e.preventDefault();
+        }
+    }
+
+    // Fourth form validation
+
+    const recMail = document.getElementById("email3");
+    const recMailFalse = document.getElementById("recMail");
+    const recPhone = document.getElementById("phone");
+    const recPhoneFalse = document.getElementById("recPhone");
+
+
+    const recSubmit = document.getElementById("recSub");
+
+    recMail.addEventListener("keypress", () => {
+       recMailFalse.style.display = "none";
+    });
+
+    recPhone.addEventListener("keypress", () => {
+        recPhoneFalse.style.display = "none";
+    });
+
+    recSubmit.addEventListener("click", checkRecForm);
+
+    function checkRecForm(e) {
+        let stop = false;
+
+        if(recMail.value.length === 0 || recMail.value.indexOf("@") < 1 || recMail.value.lastIndexOf(".") < recMail.value.lastIndexOf("@")){
+            recMailFalse.style.display = "block";
+            stop = true;
+        }
+
+        if(recPhone.value.length < 9){
+            recPhoneFalse.style.display = "block";
+            stop = true;
+        }
+
+        if(stop === true){
+            e.preventDefault();
+        }
+    }
+
+    //Fifth form validation
+
+    const subMail = document.getElementById("email4");
+    const subMailFalse = document.getElementById("subMail");
+
+    const subSubmit = document.getElementById("subSub");
+
+    subMail.addEventListener("keypress", () => {
+        subMailFalse.style.display = "none";
+    });
+
+
+    subSubmit.addEventListener("click", checkSubForm);
+
+    function checkSubForm(e) {
+        let stop = false;
+
+        if(subMail.value.length === 0 || subMail.value.indexOf("@") < 1 || subMail.value.lastIndexOf(".") < subMail.value.lastIndexOf("@")) {
+            subMailFalse.style.display = "block";
+            stop = true;
+        }
+
+        if(stop === true){
+            e.preventDefault();
+        }
+    }
 });
